f7a6d9809eb37257027f0dcfe3b213ba6f808729
Aylin <aylin@mirahi.io>
2023-03-11 17:48:25 +0100

    feat(scanner): added flashlight button

--- app/scanner.tsx
+++ app/scanner.tsx
@@ -1,7 +1,8 @@
-import { BarCodeScanningResult, Camera, CameraType } from 'expo-camera';
+import { MaterialIcons } from '@expo/vector-icons';
+import { BarCodeScanningResult, Camera, CameraType, FlashMode } from 'expo-camera';
 import { useRouter } from 'expo-router';
-import { Spinner, View } from 'native-base';
-import { useState } from 'react';
+import { Icon, IconButton, Spinner, View } from 'native-base';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 
 import { PermissionNotGranted } from '../components/scanner/PermissionNotGranted';
@@ -10,6 +11,7 @@ import { useCodeState } from '../context/code.context';
 export default function Scanner() {
   const [permission] = Camera.useCameraPermissions();
   const [scanned, setScanned] = useState(false);
+  const [isFlashOn, setFlashOn] = useState(false);
   const { setCode } = useCodeState();
 
   const { back } = useRouter();
@@ -33,10 +35,20 @@ export default function Scanner() {
   return (
     <View flex={1} justifyContent="center">
       <Camera
+        flashMode={isFlashOn ? FlashMode.torch : FlashMode.off}
         style={[styles.camera, StyleSheet.absoluteFillObject]}
         type={CameraType.back}
-        onBarCodeScanned={scanned ? () => undefined : handleBarCodeScanned}
-      />
+        onBarCodeScanned={scanned ? () => undefined : handleBarCodeScanned}>
+        <IconButton
+          alignSelf="center"
+          onPress={() => setFlashOn((s) => !s)}
+          icon={<Icon as={MaterialIcons} name={isFlashOn ? 'flash-off' : 'flash-on'} />}
+          variant={isFlashOn ? 'solid' : 'subtle'}
+          backgroundColor={isFlashOn ? undefined : 'gray.50'}
+          rounded="full"
+          size="lg"
+        />
+      </Camera>
     </View>
   );
 }
@@ -44,5 +56,7 @@ export default function Scanner() {
 const styles = StyleSheet.create({
   camera: {
     flex: 1,
+    justifyContent: 'flex-end',
+    paddingBottom: 48,
   },
 });
